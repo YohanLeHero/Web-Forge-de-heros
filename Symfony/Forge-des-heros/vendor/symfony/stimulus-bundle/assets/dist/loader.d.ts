b5a6c8c743c28150b4cb3e165cb44d8f9c574bcf
YohanLeHero <yohan.gronchi@gmail.com>
2026-03-18 15:44:23 +0100

    s

--- Symfony/Forge-des-heros/vendor/symfony/stimulus-bundle/assets/dist/loader.d.ts
+++
@@ -1,5 +0,0 @@
-import { EagerControllersCollection, LazyControllersCollection } from "./controllers.js";
-import { Application } from "@hotwired/stimulus";
-declare const loadControllers: (application: Application, eagerControllers: EagerControllersCollection, lazyControllers: LazyControllersCollection) => void;
-declare const startStimulusApp: () => Application;
-export { loadControllers, startStimulusApp };
